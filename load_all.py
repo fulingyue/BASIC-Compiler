f14438c6a14a0964ff30548eeedf1eeef5773f0e
Wenxin Zheng <peterzheng98@sjtu.edu.cn>
2020-07-28 16:19:57 +0800

    add data fetcher

---
+++ load_all.py
@@ -0,0 +1,27 @@
+import pickle
+
+file_template_list = ['testcases/basic_test/basic_{}.txt', 'testcases/control_test/control_{}.txt', 'testcases/op_test/op_{}.txt']
+
+basic_size = 10
+control_size = 5
+op_size = 10
+
+file_list = []
+for i in range(basic_size):
+    file_list.append(file_template_list[0].format(i + 1))
+
+for i in range(control_size):
+    file_list.append(file_template_list[1].format(i + 1))
+
+for i in range(op_size):
+    file_list.append(file_template_list[2].format(i + 1))
+
+test_case = []
+for f in file_list:
+    all_lines = open(f, 'r').readlines()
+    file_name = all_lines[0].split(' ')[2]
+    input_data = all_lines[1].split(':')[1].strip()
+    exit_code = all_lines[2].split(':')[1].strip()
+    test_case.append((file_name.strip('\n'), ''.join(input_data).strip('\n'), int(exit_code), '{}\n'.format(''.join(all_lines))))
+    print('Finish: ', f)
+
