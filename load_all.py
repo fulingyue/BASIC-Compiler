6da0e818c9fdecbb77bd318cf57ea66d7abde2f2
Wenxin Zheng <peterzheng98@sjtu.edu.cn>
2020-08-12 15:08:38 +0800

    update data

--- load_all.py
+++ load_all.py
@@ -1,21 +1,29 @@
 import pickle
 import base64
 
-file_template_list = ['testcases/basic_test/basic_{}.txt', 'testcases/control_test/control_{}.txt', 'testcases/op_test/op_{}.txt']
+file_template_list = ['testcases/array_test/array_{}.txt', 'testcases/basic_test/basic_{}.txt', 'testcases/control_test/control_{}.txt', 'testcases/op_test/op_{}.txt', 'testcases/sema/sema_{}.txt']
 
+array_size = 5
 basic_size = 10
 control_size = 5
 op_size = 10
+sema_size = 14
 
 file_list = []
-for i in range(basic_size):
+for i in range(array_size):
     file_list.append(file_template_list[0].format(i + 1))
 
-for i in range(control_size):
+for i in range(basic_size):
     file_list.append(file_template_list[1].format(i + 1))
 
-for i in range(op_size):
+for i in range(control_size):
     file_list.append(file_template_list[2].format(i + 1))
+
+for i in range(op_size):
+    file_list.append(file_template_list[3].format(i + 1))
+
+for i in range(sema_size):
+    file_list.append(file_template_list[4].format(i + 1))
 
 test_case = []
 for f in file_list:
@@ -24,7 +32,7 @@ for f in file_list:
     input_data = all_lines[1].split(':')[1].strip()
     exit_code = all_lines[2].split(':')[1].strip()
     all_lines[2] = '3 REM asfeskejfewf'
-    test_case.append((file_name.strip('\n'), ''.join(input_data).strip('\n'), int(exit_code), '{}\n'.format(''.join(all_lines))))
+    test_case.append((file_name.strip('\n'), ''.join(input_data).strip('\n'), -1 if 'FAIL' in exit_code else int(exit_code), '{}\n'.format(''.join(all_lines))))
     print('Finish: ', f)
 
 s = pickle.dumps(test_case)
